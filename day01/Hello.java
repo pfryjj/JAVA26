aaa2006929843ffcae75ce06d3b58284cb285c1b
pfryjj <61343864+pfryjj@users.noreply.github.com>
2026-08-19 16:14:49 +0900

    Add files via upload

---
+++ day01/Hello.java
@@ -0,0 +1,12 @@
+package day01;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch2 = 'Y';
+		System.out.println(ch2);
+		
+	}
+
+}
