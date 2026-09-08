ffa6b5176b5ceb29791150b73d1836983220a9ea
pfryjj <61343864+pfryjj@users.noreply.github.com>
2026-09-08 12:15:42 +0900

    Add files via upload

---
+++ day03/src/homework/chap5/exam05_01.java
@@ -0,0 +1,24 @@
+package homework;
+
+public class exam05_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String test = "cccccccc";
+		char search = 'c';
+		int result = countChar(test, search);
+		
+		System.out.print(test + "에서 c의 개수는" + result + "입니다");
+		
+	}
+	public static int countChar(String s, char c) {
+		int count = 0;
+		
+		for(int i = 0; i < s.length(); i++) {
+				if(s.charAt(i) == c) {
+					count += 1;
+				}
+			}
+		return count;
+	}
+}
