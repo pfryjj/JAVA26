af1bc90ba6e9652747d4e632df582e202c2cd63a
pfryjj <61343864+pfryjj@users.noreply.github.com>
2026-09-08 18:03:00 +0900

    Add files via upload

---
+++ day03/src/homework/chap03/challenge03_03.java
@@ -0,0 +1,26 @@
+package homework;
+
+public class challenge03_03 {
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+        System.out.println(factorial(1, 5));
+        System.out.println(factorial(3, 5));
+        System.out.println(factorial(10, 5));
+    }
+
+    static int factorial(int x) {
+        int r = 1;
+        while (x > 0) {
+            r *= x--;
+        }
+        return r;
+    }
+
+    static int factorial(int x, int y) {
+        int r = 1;
+        while (y >= x) {
+            r *= y--;
+        }
+        return r;
+    }
+}
