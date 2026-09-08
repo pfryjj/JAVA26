0257aca052b6663f70367ba9808fc6d373678d8c
pfryjj <61343864+pfryjj@users.noreply.github.com>
2026-09-08 18:03:20 +0900

    Add files via upload

---
+++ day03/src/homework/chap5/challenge05_01.java
@@ -0,0 +1,27 @@
+package homework;
+
+import java.util.Scanner;
+
+public class challenge05_01 {
+    public static void main(String[] args) {
+        int numOfStudents = 0;
+        int[] scores;
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("학생 수? ");
+        numOfStudents = in.nextInt();
+        scores = new int[numOfStudents];
+
+        System.out.println(numOfStudents + "명의 학생 성적을 입력하세요.");
+        for (int i = 0; i < numOfStudents; i++) {
+            scores[i] = in.nextInt();
+        }
+
+        System.out.println(numOfStudents + "명의 학생 성적은 다음과 같습니다.");
+        for (int i = 0; i < numOfStudents; i++) {
+            System.out.print(scores[i] + " ");
+        }
+        
+        System.out.println(); 
+    }
+}
