50d42c7e452cb295feaddc33a6c365d5d846b245
pfryjj <61343864+pfryjj@users.noreply.github.com>
2026-09-09 20:55:23 +0900

    Add files via upload

---
+++ day04/circle/Circle.java
@@ -0,0 +1,22 @@
+package circle;
+
+public class Circle {
+	static final double PI = 3.141592;
+	private double radius;
+	
+	public Circle(double radius) {
+		this.radius = radius;
+	}
+	
+	public double getArea() {
+		return radius * radius * PI;
+	}
+	
+	public double getRadius() {
+		return radius;
+	}
+	
+	static public double getArea(double radius) {
+		return radius * radius * PI;
+	}
+}
